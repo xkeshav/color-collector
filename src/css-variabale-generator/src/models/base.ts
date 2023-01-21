af73ef4cffb610737586c7df0691f821f1d9501d
Keshav Mohta <xkeshav@gmail.com>
2023-01-21 22:57:39 +0530

    remove vs code extension code, put in different repo

--- src/css-variabale-generator/src/models/base.ts
+++
@@ -1,5 +0,0 @@
-export type VariableList = {
-	[key: string]: string
-};
-
-export type RegExpMatchArrayWithIndices = RegExpMatchArray & { indices: Array<[number, number]> & { groups: { [key: string]: [number, number] } } };
